117597462184342486a50196674888cae737caa5
MilozVirus <cami_alejo6@hotmail.com>
2022-07-08 12:21:06 -0500

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+class Player
+{
+    constructor(playerName, playerId, game, status)
+    {
+        this.playerName = playerName
+        this.playerId = playerId
+        this.game = game
+        this.status = status
+    }
+    generateId()
+    {
+        
+    }
+}
